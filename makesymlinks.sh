e1b9ad9e1fac6f62e2608b38b158532810ea6b80
Erez Shermer <eshermer@qspark.co>
2015-04-29 14:04:06 +0300

    Added submodule update

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -23,6 +23,8 @@ echo -n "Changing to the $dir directory ..."
 cd $dir
 echo "done"
 
+git submodule update --init
+
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
 for file in $files; do
     echo "Moving any existing dotfiles from ~ to $olddir"
