2c4eb472996b5544e4219e2076d23a9ff89d1438
Erez Shermer <erezshermer@gmail.com>
2016-06-16 17:54:18 +0300

    Improvements

--- pre_install.sh
+++ pre_install.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 rm -rf ~/dotfiles_old
 
+if [[ ! -f /bin/zsh && ! -f /usr/bin/zsh ]]; then
+	echo "ZSH not found, will try to install it"
+	if [[ -n "$(command -v apt-get)" ]]; then
+		sudo apt-get install zsh
+	else
+		sudo yum install zsh
+	fi
+fi
+
